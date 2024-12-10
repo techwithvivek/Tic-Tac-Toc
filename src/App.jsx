bfdbc91e8821801fd3128bd686f852f7a49a3d5b
Vivek Bharti <1vivekbharti@gmail.com>
2024-12-10 15:53:51 +0530

    recat

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TicTacToe from "./TicTacToe"
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className="App">
+      <TicTacToe/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/TicTacToe.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+
+function TicTacToe() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXTurn, setXTurn] = useState(true);
+  const [winner, setWinner] = useState(null);
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const handleClick = (index) => {
+    if (board[index] != null || winner) {
+      return;
+    }
+    const newBoard = [...board];
+    newBoard[index] = isXTurn ? "X" : "O";
+    setBoard(newBoard);
+    setXTurn(!isXTurn);
+
+    const winnerCombination = checkwinner(newBoard);
+    if (winnerCombination) {
+      setWinner(newBoard[winnerCombination[0]]);
+    }
+  };
+
+  const checkwinner = (newBoard) => {
+    const combination = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < combination.length; i++) {
+      const [a, b, c] = combination[i];
+      if (newBoard[a] &&  newBoard[b] && newBoard[b] === newBoard[c]) {
+        return combination[i];
+      }
+    }
+    return null;
+  };
+
+  const handleReset = () => {
+    setBoard(Array(9).fill(null));
+    setWinner(null);
+    setXTurn(true);
+  };
+
+  return (
+    <>
+    <div className="board-container">
+      <div className="board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <button className="rest-btn" onClick={handleReset}>
+        Reset
+      </button>
+      {winner && <div>{winner} is the winner of this Game.</div>}
+      </div>
+    </>
+  );
+}
+
+export default TicTacToe;
